67374efcd2fb4970de531cfd26eaa7da9c7d5219
Joshua Blackman <joshuadennisblackman@gmail.com>
2018-12-28 23:34:22 +0000

    Initial Commit

---
+++ CameraTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CameraTest
+//
+//  Created by Josh on 28/12/2018.
+//  Copyright © 2018 Alex. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
